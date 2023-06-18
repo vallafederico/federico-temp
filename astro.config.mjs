c11e6408407073e2be284332c20311ec048be393
vallafederico <valla.federico@gmail.com>
2023-06-18 23:18:24 +0200

    added webgl and animation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import glsl from "vite-plugin-glsl";
 
 // https://astro.build/config
 export default defineConfig({
