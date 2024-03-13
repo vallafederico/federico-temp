e6ca35e501080aaed99d45089fd584a93a6ff2e7
vallafederico <valla.federico@gmail.com>
2024-03-13 18:26:34 +0100

    migratio branch

---
+++ redirects.js
@@ -0,0 +1,14 @@
+export const redirects = {
+  "/s/twitter": "https://twitter.com/fdrc_o",
+  "/s/instagram": "https://www.instagram.com/fpalla/",
+  "/s/linkedin": "https://www.linkedin.com/in/federicovalla/",
+  "/s/behance": "https://www.behance.net/FedericoValla",
+  "/s/dribbble": "https://dribbble.com/federicooo",
+  "/s/savee": "https://savee.it/federicooo/",
+  "/s/cosmos": "https://www.cosmos.so/federicooo",
+  "/s/youtube": "https://www.youtube.com/@fd.o",
+  "/s/awwwards": "https://www.awwwards.com/fvalla/",
+  "/s/twitch": "https://www.twitch.tv/247246245",
+  "/s/github": "https://github.com/vallafederico",
+  "/s/codesandbox": "https://codesandbox.io/u/vallafederico",
+};
