c5aa16959b53ef8dddf2bd36293f4f6967d2c319
Federico Valla <38297526+vallafederico@users.noreply.github.com>
2024-11-14 15:28:20 +0100

    updated youtube handle

--- redirects.js
+++ redirects.js
@@ -6,7 +6,7 @@ export const redirects = {
   "/s/dribbble": "https://dribbble.com/federicooo",
   "/s/savee": "https://savee.it/federicooo/",
   "/s/cosmos": "https://www.cosmos.so/federicooo",
-  "/s/youtube": "https://www.youtube.com/@fd.o",
+  "/s/youtube": "https://www.youtube.com/@ffmfed",
   "/s/awwwards": "https://www.awwwards.com/fvalla/",
   "/s/twitch": "https://www.twitch.tv/247246245",
   "/s/github": "https://github.com/vallafederico",
